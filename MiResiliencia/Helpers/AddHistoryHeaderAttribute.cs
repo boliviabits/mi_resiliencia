7e40cf2c5c93edad989e2bd6c5705cbf10e71fce
Christoph Suter <Christoph.Suter@geotest.ch>
2023-08-07 18:00:21 +0200

    Test for Azure-Headers

--- MiResiliencia/Helpers/AddHistoryHeaderAttribute.cs
+++ MiResiliencia/Helpers/AddHistoryHeaderAttribute.cs
@@ -9,6 +9,7 @@ namespace MiResiliencia.Helpers
             string app = filterContext.HttpContext.Request.Host.Value;
             string tool = filterContext.HttpContext.Request.Path;
             Console.WriteLine(filterContext.HttpContext.Request.QueryString.Value);
+            Console.WriteLine(filterContext.HttpContext.Request.Path);
             if ((filterContext.HttpContext.Request.QueryString.Value.Contains("ic-request=true")) && (!tool.Contains("Admin")))
                 filterContext.HttpContext.Response.Headers.Append("X-IC-PushURL", "?inside=" + tool + "#home");
 
